537feb00377d60327281ddc0911235bcae78e662
Shivam Shukla <123316496+shivamshuklah@users.noreply.github.com>
2023-11-17 23:07:56 +0530

    Update controller.dart

--- lib/controller/controller.dart
+++ lib/controller/controller.dart
@@ -3,7 +3,7 @@ import 'package:get/get.dart';
 
 
 class MyController extends GetxController {
-  // Define your variable that can be changed and used from anywhere
+  /// Define your variable that can be changed and used from anywhere
   var myVariable = 'Lucknow'.obs;
 }
 
